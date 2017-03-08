749e511022429c64897a8bbe8d55d7c8ea4c5815
Daniel Tonon <daniel.tonon@adelphi.digital>
2017-03-08 20:17:37 +1100

    emphasised the "is"

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -7,7 +7,7 @@ import $ from 'jquery';
 import 'typeahead.js/dist/typeahead.jquery.js';
 
 if ($.fn.typeahead){
-  $('body').append('<p style="text-align: center; font-size: 30px;">typeahead <strong>is</strong> defined in jQuery.fn</p>');
+  $('body').append('<p style="text-align: center; font-size: 30px;">typeahead <strong>IS</strong> defined in jQuery.fn</p>');
 } else {
   $('body').append('<p style="text-align: center; font-size: 30px;">typeahead is <strong>NOT</strong> defined in jQuery.fn</p>');
 }
